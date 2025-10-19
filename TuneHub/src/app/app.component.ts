35b7a2fb90870dfb5dd081af2bade7c52cc80355
shira-katzinelbogen <shira.fk2006@gmail.com>
2025-10-19 21:47:18 +0300

    shiraChanges

--- TuneHub/src/app/app.component.ts
+++ TuneHub/src/app/app.component.ts
@@ -3,9 +3,15 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './Components/Shared/navbar/navbar.component';
 import { SidebarComponent } from './Components/Shared/sidebar/sidebar.component';
 
+// --- ייבוא חובה של Material ---
+import { MatSidenavModule } from '@angular/material/sidenav'; 
+import { FooterComponent } from './Components/Shared/footer/footer.component';
+// --------------------------------
+
+// ודא שגם הקומפוננטות שלך מיובאות אם הן Standalone:
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,NavbarComponent,SidebarComponent],
+  imports: [RouterOutlet,NavbarComponent,SidebarComponent,MatSidenavModule,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
